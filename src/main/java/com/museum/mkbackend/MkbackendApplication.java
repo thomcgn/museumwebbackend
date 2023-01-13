37851e305a6912e95cd92ce768b8de9ee8248e20
Tom <55287388+thomcgn@users.noreply.github.com>
2023-01-13 17:28:23 +0100

    Museum Kneipe Website Backend Day#1

---
+++ src/main/java/com/museum/mkbackend/MkbackendApplication.java
@@ -0,0 +1,13 @@
+package com.museum.mkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MkbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/museum/mkbackend/MkbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.museum.mkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
